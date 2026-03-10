b02a08350eb73b652ced853e93dd6966cc6303d3
Vishnu <Vishnu.vm235@gmail.com>
2026-03-10 19:42:03 +0530

    slider image is add

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php $page = 'contact'; include 'include/header.php'; ?>
+
+    <main class="main-content">
+        <!-- Contact Section -->
+        <?php include 'form.php'; ?>
+    </main>
+
+    <!-- Footer -->
+<?php include 'include/footer.php'; ?>
+
